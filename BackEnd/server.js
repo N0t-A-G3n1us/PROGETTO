91a023826b28ad85375f3ac7cfb192e0ca35c937
Giuseppe <romagiuppo@gmail.com>
2017-11-22 08:58:09 +0100

    Earthquake

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,2 +1,19 @@
+var express=require('express');
+var app=express();
+
 console.log("dovrei essere un server");
 
+app.get("/", function (req,res) {
+	
+	res.send("Questa pagina è stata creata con express [tachius]");
+	})
+	
+
+var server = app.listen(8081,function() {
+	
+	var host = server.address().address
+	var port = server.address().port
+	
+	console.log("app in ascolto at http://%s:%s",host,port);
+	
+	})
