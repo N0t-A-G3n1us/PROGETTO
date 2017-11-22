aa7d2a68dbeffb48b5498b4f77ec5db2759bf413
Giuseppe <romagiuppo@gmail.com>
2017-11-22 11:21:30 +0100

    Gino

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,19 @@
+var http=require('http');
 var express=require('express');
+
+
+
 var app=express();
+
+
 
 console.log("dovrei essere un server");
 
 app.get("/", function (req,res) {
 	
 	res.send("Questa pagina è stata creata con express [tachius]");
-	})
+	
+		})
 	
 
 var server = app.listen(8081,function() {
