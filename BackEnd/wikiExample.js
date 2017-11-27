a0b9e8d1721b1b0b25e168d678d2c6453ba43696
Unknown <capozzigianluca96@gmail.com>
2017-11-27 11:48:52 +0100

    wikiex

--- BackEnd/wikiExample.js
+++ BackEnd/wikiExample.js
@@ -8,24 +8,30 @@ var bodyParser=require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-var url1="https://en.wikipedia.org/w/api.php?action=query&titles=";
+//var url1="https://en.wikipedia.org/w/api.php?action=query&titles=";
 
 var citta="Milan";
-var url2="&prop=revisions&rvprop=content&format=json";
-var url=url1+citta+url2
+//var url2="&prop=revisions&rvprop=content&format=json";
+
+//var url=url1+citta+url2
+var stackUrl="https://en.wikipedia.org/w/api.php?format=json&"+
++"action=query&prop=extracts&exintro=&explaintext=&titles="+citta;
 
 
 
 
 	
-	function callback(error,res,body){
-		var info=JSON.parse(body)
-		console.log(info.query.pages);
-		}
+function callback(error,res,body){
+	var info=JSON.parse(body);
+	let page = info.query.pages;
+	let pageId = Object.keys(info.query.pages)[0];
+	let content = pageId.extract;
+	console.log(content);
+}
 
 
 	
-request.get(url,callback)
+request.get(stackUrl,callback)
 
 
 var server = app.listen(3000,function() {
