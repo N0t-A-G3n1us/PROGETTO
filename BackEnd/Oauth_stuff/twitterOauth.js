9cb78e9b21c60a159ca8a3c6ce301050fbb96a99
Gianluca <capozzigianluca96@gmail.com>
2017-11-30 20:49:32 +0100

    Auto stash before merge of "HTMLgame" and "origin/HTMLgame"

--- BackEnd/Oauth_stuff/twitterOauth.js
+++ BackEnd/Oauth_stuff/twitterOauth.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app=express();
 
 
@@ -22,10 +22,31 @@ app.get("/",function(req,res){
 
 app.get('*', function(req, res){
     res.redirect('/login');
-});	
-	
+});
+*/
 
-var server=app.listen(3000,function(){
+var OAuth = require('OAuth');
+var oauth = new OAuth.OAuth(
+	 'https://api.twitter.com/oauth/request_token',
+	 'https://api.twitter.com/oauth/access_token',
+	 'IPbVAysd5FgG8f05raz1xi9YP',
+	 'IzYVFgxnt8UZAGipRz2zLP8MPiWLovEafsGEsN8CdhgDaA1WMi',
+	 '1.0A',
+	 null,
+	 'HMAC-SHA1'
+	);
+oauth.get(
+     'https://api.twitter.com/1.1/trends/place.json?id=934169103526490114',
+     '934169103526490114-trZFgqQ7u2j1nfL39tCZQThaLcu5flm',
+     '5Q0P2SMcYyLdVrLtCUGhqxqbxoLETSpbi3ScxDl2a67B9',
+     function(e, data, res){
+     	if(e) console.error(e);
+     	console.log(require('util').inspect(data));
+     	done();
+     }
+);
+
+/*var server=app.listen(3000,function(){
 	
 	var host =server.address().adress
 	var port =server.address().port
@@ -33,4 +54,5 @@ var server=app.listen(3000,function(){
 	console.log("Server running at "+host +" "+ port); 
 	
 	
-	})
+});
+*/
