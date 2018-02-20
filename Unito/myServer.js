3f2e908f8dbba086ac4cf3bcb93c18766e996db4
Giuseppe <romagiuppo@gmail.com>
2018-02-20 12:15:51 +0100

    2.0: Auto stash before merge of "HtmlGame2.0" and "origin/HTMLgame"

--- Unito/myServer.js
+++ Unito/myServer.js
@@ -1,9 +1,13 @@
+//moduli oauth
+var OAuth = require('oauth');
+
 //moduli server
 var request=require('request');
 var express=require('express');
-//var events=require('events');
-//var eventEmitter = new events.EventEmitter();
 var app=express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 //moduli websocket
 var server = require('ws').Server;  
@@ -16,29 +20,11 @@ flickrOptions = {
   //secret necessario solo se si vogliono usare metodi authenticated (OAuth)
    secret: "a2af028ce7e6faae"
 };
+
 //moduli itunes
 var itunes = require('itunes-search')
 
-
-const myport=5000;
-
-/////////////////////////////////     ISTRUZIONI
-/*
-istruzioni:
- lanciare ws_render_server.js
-  andare su localhost:3000/showData/:nomeCosaDaCercare
-  wait
-  
-*/
-////////////////////////////////////////////////////////////////
-
-
-
-
-
-//var latitude= 45.4642035
-//var longitude= 9.189981999999986
-//const radius= 6  // tra i 5 e i 32
+const myPort=5000;
 
 var images; // var contentente url delle imgs flickr
 var wikiStr;  // var contenente res della wiki search
@@ -52,7 +38,6 @@ var jsonDescs;
 
 var elToSearch="";
 
-//var dataArrived=false;  //var globale per sapere quando i dati sono arrivati
 
 function getData(){
 
@@ -190,10 +175,164 @@ function getData(){
 
 } //fine di getData()
 
-
 app.get('/get',function(req,res){
   	res.sendFile("/home/giuppo/Desktop/PROJ-X_RC/Unito/feRender.html");
 });
+
+
+
+
+
+    
+
+
+
+
+
+
+/////////////// home for auth
+
+app.get('/homepage', function(req, res){
+    res.sendFile( "/home/giuppo/Desktop/PROJ-X_RC/Unito/fe.html");
+
+});
+
+
+
+
+////////////////	Twitter
+
+
+var oauth = new OAuth.OAuth(
+		 "https://api.twitter.com/oauth/request_token",
+		 "https://api.twitter.com/oauth/access_token",
+		 "IPbVAysd5FgG8f05raz1xi9YP",
+		 "IzYVFgxnt8UZAGipRz2zLP8MPiWLovEafsGEsN8CdhgDaA1WMi",
+		 "1.0",
+		 "http://127.0.0.1:"+myPort+"/auth/twitter/callback",
+		 "HMAC-SHA1"
+		);
+
+
+
+app.use(session({ secret: 'IzYVFgxnt8UZAGipRz2zLP8MPiWLovEafsGEsN8CdhgDaA1WMi',
+				 cookie: { maxAge: 60000 }}))
+
+
+app.get('/auth/twitter', function(req, res){
+	console.log("in auth twitter");
+	oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+		if(error){
+			res.send("Error getting oauth request token");
+		}
+		else{
+			console.log("ready to redirect on https://twitter.com/oauth/authenticate?oauth_token=XXX");			
+			req.session.oauth = {
+	        token: oauth_token,
+	        token_secret: oauth_token_secret
+	        
+	     	 };
+      		console.log(req.session.oauth);
+
+			res.redirect("https://twitter.com/oauth/authenticate?oauth_token=" + oauth_token);
+		}
+	});
+});
+
+app.get('/auth/twitter/callback', function(req, res, next){
+	
+	console.log("\n"+ "verifying Authentication")
+
+
+	if(req.session.oauth){
+		req.session.oauth.verifier = req.query.oauth_verifier;
+		var oauth_data = req.session.oauth;
+		oauth.getOAuthAccessToken(oauth_data.token, 
+			oauth_data.token_secret,
+			oauth_data.verifier,
+			function(error, oauth_access_token, oauth_access_token_secret, results){
+			
+				if(error) new Error(error);
+				else{
+					req.session.oauth.access_token = oauth_access_token;
+					req.session.oauth.access_token_secret = oauth_access_token_secret;
+					console.log(results, req.session.oauth);
+					//res.send("Authentication succesful!")
+					res.redirect("http://localhost:"+myPort+"/get");
+				}
+		});
+	}
+	else{
+		next(new Error('No OAuth info stored in the session'));
+	}
+});
+
+
+
+
+
+///////////			Google          //////////
+
+var a_t = '';
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/auth/google', function(req, res){
+	console.log("in login google");
+	res.redirect("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/drive.metadata.readonly&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=http://localhost:"+myPort+"/auth/google/callback&response_type=code&client_id=649610250091-2gjifn38g27d7rc84eg35bbkst478a52.apps.googleusercontent.com");
+});
+
+app.get('/auth/google/callback', function(req, res){
+	console.log("code taken");
+	var formData = {
+		code: req.query.code,
+		client_id: '649610250091-2gjifn38g27d7rc84eg35bbkst478a52.apps.googleusercontent.com',
+		client_secret: 'SXzNSSJbekgoCIEXWQkjtzlR',
+		redirect_uri: 'http://localhost:'+myPort,
+		grant_type: 'authorization_code',
+	}
+	request.post({url: 'https://accounts.google.com/o/oauth2/token', form: formData}, function optionalCallback(err, httpResponse, body){
+		if(err){
+			return console.error('upload failed: ', err);
+		}
+		console.log('Upload successful! Server responded with: ', body);
+		var info = JSON.parse(body);
+		//res.send("Got the token: " + info.access_token);
+		res.redirect(formData.redirect_uri+"/get");
+		a_t = info.access_token;
+	});
+});
+
+app.get('/use_token', function(req, res){
+	var options ={
+		url: 'https://www.googleapis.com/drive/v2/files',
+		headers: {
+			'Authorization' : 'Bearer ' + a_t
+		}
+	};
+	request(options, function callback(error, response, body){
+		if(!error && response.statusCode == 200){
+			var info = JSON.parse(body);
+			console.log(info);
+			res.send(info);
+		}
+		else{
+			console.log(error);
+		}
+	});
+});
+
+app.get('/home_logged',function(req,res){
+	res.send("You logged successfully from: "+ req.query.from);
+
+});
+
+
+app.get('*',function(req,res){
+
+	res.redirect("/homepage");
+});
+
+
 
 
 
@@ -271,15 +410,9 @@ s.on('connection',function(ws,req){
 	
 
 
-
-    
-
-
-
-
 /////////////////////////////// SERVER START PART
 
-var server = app.listen(myport,function() {
+var server = app.listen(myPort,function() {
   
   var host = server.address().address
   var port = server.address().port
