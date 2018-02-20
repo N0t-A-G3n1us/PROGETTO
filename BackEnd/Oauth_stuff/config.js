282cc78a9f5fb856ae773c82f0f22ea561d111fd
Giuseppe <romagiuppo@gmail.com>
2018-02-20 22:30:35 +0100

    a

--- BackEnd/Oauth_stuff/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'HOSTPATH': 'http://localhost:5000/',
-  'PORT': 5000,
-  'EXPRESS_SESSION_SECRET': '123456',
-  'TWITTER_CONSUMER_KEY': 'IPbVAysd5FgG8f05raz1xi9YP',
-  'TWITTER_CONSUMER_SECRET': 'IzYVFgxnt8UZAGipRz2zLP8MPiWLovEafsGEsN8CdhgDaA1WMi',
-  'GOOGLE_APP_ID': '649610250091-2gjifn38g27d7rc84eg35bbkst478a52.apps.googleusercontent.com',
-  'GOOGLE_CONSUMER_SECRET': 'SXzNSSJbekgoCIEXWQkjtzlR',
-};
